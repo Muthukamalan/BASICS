227168c67731ff641688c1e75df6da7483031acc
Muthu Kamalan <muthukamalan98@gmail.com>
2024-02-10 23:53:39 +0530

    chapter 3 init

--- ML Revisit/Revisit ML/init.py
+++ ML Revisit/Revisit ML/init.py
@@ -4,8 +4,15 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
 import seaborn as sns 
 import scipy
+
 import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
 
 plt.style.use('ggplot')
 sns.set_context('talk')
 sns.color_palette("hls", 8)
+
+from pprint import pprint
